807e8564ebf87b78f211a92f047ff18a3f7e13be
Hazelando <a710210013@student.ums.ac.id>
2024-06-17 10:49:31 +0700

    day 5
    30 hari python

---
+++ hari 5.py
@@ -0,0 +1,6 @@
+def umur_anjing(umur_manusia):
+    umur_anjing = umur_manusia * 7
+    return umur_anjing
+
+umur_manusia = int(input("Masukkan usia manusia: "))
+print("Usia manusia dalam tahun anjing adalah:", umur_anjing(umur_manusia))

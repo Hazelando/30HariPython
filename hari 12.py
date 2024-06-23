87e31ce4f169795c2a9e0153c9c737fcb2079ee6
Hazelando <a710210013@student.ums.ac.id>
2024-06-24 00:16:51 +0700

    day 12
    30 hari python

---
+++ hari 12.py
@@ -0,0 +1,8 @@
+def celsius_ke_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_ke_celsius(f):
+    return (f - 32) * 5/9
+
+print(celsius_ke_fahrenheit(30))
+print(fahrenheit_ke_celsius(86))
